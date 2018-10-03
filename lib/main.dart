a57f0af17de5c1931830753f05b9f41f7fe8487b
Khan Fan <khan.fan@oriente.com>
2018-10-03 13:00:41 +0800

    pass params through props: widget & constructor

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('EasyList demo'),
         ),
-        body: ProductManager(),
+        body: ProductManager('Food Tester'),
       ),
     );
   }
