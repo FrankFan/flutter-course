84d4dada10697aebca479986c2d9280a06a7e52f
Khan Fan <khan.fan@oriente.com>
2018-10-03 12:51:01 +0800

    reconstruction to seperate files

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,9 @@
 import 'package:flutter/material.dart';
+import './product_manager.dart';
 
 main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    return _MyAppState();
-  }
-}
-
-class _MyAppState extends State<MyApp> {
-  List<String> _products = ['Food Tester'];
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -20,35 +11,7 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: Text('EasyList demo'),
         ),
-        body: Column(
-          children: [
-            Container(
-              margin: EdgeInsets.all(20.0),
-              child: RaisedButton(
-                onPressed: () {
-                  setState(() {
-                    _products.add('Advanced Food Tester');
-                  });
-                },
-                child: Text('Add Product'),
-              ),
-            ),
-            Column(
-              children: _products
-                  .map(
-                    (element) => Card(
-                          child: Column(
-                            children: <Widget>[
-                              Image.asset('assets/food.jpg'),
-                              Text(element),
-                            ],
-                          ),
-                        ),
-                  )
-                  .toList(),
-            ),
-          ],
-        ),
+        body: ProductManager(),
       ),
     );
   }
