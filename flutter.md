7f4105e0078edbba4dcc4b4b6522ccec22097afb
Khan Fan <khan.fan@oriente.com>
2018-10-03 20:19:10 +0800

    update

--- flutter.md
+++ flutter.md
@@ -1,7 +1,5 @@
 ## Flutter
 
-> https://srv65.clipconverter.cc/download/qpyXqaeKoGuwZ2trmpqYaGtp5KWmqW9t4pSYa3FkoGNlamm0qc%2FMqHyf1qiZpa2d2A%3D%3D/Flutter%20Tutorial%20for%20Beginners%20-%20Build%20iOS%20and%20Android%20Apps%20with%20Google%27s%20Flutter%20u0026%20Dart.mp4
-
 
 Dart
 |
--- lib/product_manager.dart
+++ lib/product_manager.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import './products.dart';
+import './product_control.dart';
 
 class ProductManager extends StatefulWidget {
   final String startingProduct;
@@ -18,7 +19,7 @@ class ProductManager extends StatefulWidget {
 }
 
 class _ProductManagerState extends State<ProductManager> {
-  List<String> _products = [];
+  final List<String> _products = [];
 
   @override
   void initState() {
@@ -33,24 +34,22 @@ class _ProductManagerState extends State<ProductManager> {
     super.didUpdateWidget(oldWidget);
   }
 
+  void _addProduct(String product) {
+    setState(() {
+      _products.add(product);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     print('[ProductManager State] build()');
     return Column(
       children: [
-        Container(
+        Container (
           margin: EdgeInsets.all(20.0),
-          child: RaisedButton(
-            color: Theme.of(context).primaryColor,
-            onPressed: () {
-              setState(() {
-                _products.add('Advanced Food Tester');
-              });
-            },
-            child: Text('Add Product'),
-          ),
+          child: ProductControl(_addProduct),
         ),
-        Products(_products)
+        Products(_products),
       ],
     );
   }
