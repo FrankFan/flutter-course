9245f8382f3cc12da6e38f42e1519fa9d9588cb3
Khan Fan <khan.fan@oriente.com>
2018-10-03 19:45:31 +0800

    setState example

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import './product_manager.dart';
+import './text_control.dart';
 
 main() => runApp(MyApp());
 
@@ -7,16 +7,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        brightness: Brightness.light,
-        primarySwatch: Colors.deepOrange,
-        accentColor: Colors.deepPurple,
-      ),
       home: Scaffold(
         appBar: AppBar(
-          title: Text('EasyList demo'),
+          title: Text('Assignment 1'),
         ),
-        body: ProductManager(startingProduct: 'Food Tester'),
+        body: TextControl(),
       ),
     );
   }
